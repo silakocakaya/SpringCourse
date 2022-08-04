2af3c4e7cf8a20adcd64b9bd3070821a5c83f2e4
silakocakaya <67272114+silakocakaya@users.noreply.github.com>
2022-08-04 17:13:20 +0300

    prototype scope for predest

--- SpringCourse/src/main/java/annotationBased/qualifiers/SadFortuneService.java
+++ SpringCourse/src/main/java/annotationBased/qualifiers/SadFortuneService.java
@@ -1,12 +1,37 @@
 package annotationBased.qualifiers;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/**
+ * prototype scope'unda destroy methodu otomatik cagrilmiyor.
+ * Ek islemler gerekiyor.
+ * 
+ *
+ */
 @Component
+@Scope("prototype")
 public class SadFortuneService implements FortuneService {
 
 	public String getFortune() {
 		return "Today is a sad day";
 	}
+	
+	@PostConstruct
+	public void runPostConstruct() {
+		System.out.println();
+		System.out.println("---SadFortuneService PostConstruct");
+	}
+	
+	@PreDestroy
+	public void runPreDestroy() {
+		System.out.println();
+		System.out.println("--SadFortuneService PreDestroy");
+	}
+	
+	
 
 }
